e4125a530eff68487bf605365bc64af629f49952
Zmiter <ozmiter@gmail.com>
2021-12-03 01:58:19 +0300

    feat: add fastify running server

--- src/app.js
+++ src/app.js
@@ -1,24 +1,56 @@
 const express = require('express');
-const swaggerUI = require('swagger-ui-express');
+ const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
+
+// const Fastify = require('fastify');
+// const ExpressPlugin = require('fastify-express');
 const userRouter = require('./resources/users/user.router');
 
-const app = express();
-const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
-app.use(express.json());
 
-app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+   const app = express();
+   const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
 
-app.use('/', (req, res, next) => {
-  if (req.originalUrl === '/') {
-    res.send('Service is running!');
-    return;
-  }
-  next();
-});
+  app.use(express.json());
 
-app.use('/users', userRouter);
+   app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
-module.exports = app;
+  app.use('/', (req, res, next) => {
+    if (req.originalUrl === '/') {
+      res.send('Service is running!');
+      return;
+    }
+    next();
+  });
+  app.use('/users', userRouter);
+
+
+
+
+ module.exports = app;
+// const app2 = async () => {
+//   const app = Fastify({
+//     logger: true,
+//   });
+//   await app.register(ExpressPlugin);
+//
+//   // const app = express();
+//   // const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
+//
+//   app.use(express.json());
+//
+//   // app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+//
+//   app.use('/', (req, res, next) => {
+//     if (req.originalUrl === '/') {
+//       res.send('Service is running!');
+//       return;
+//     }
+//     next();
+//   });
+//   app.use('/users', userRouter);
+//   return app;
+// };
+// module.exports = app2();
+
--- src/server.js
+++ src/server.js
@@ -1,6 +1,16 @@
 const { PORT } = require('./common/config');
 const app = require('./app');
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+// app.listen(PORT, () =>
+//   console.log(`App is running on http://localhost:${PORT}`)
+// );
+(async () => {
+    const asApp = await app
+    try {
+        await asApp.listen(PORT)
+    } catch (err) {
+        asApp.log.error(err)
+        process.exit(1)
+    }
+})()
+
