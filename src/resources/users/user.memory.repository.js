cd117a33c6661e5df68c70d2d1213110fd556c29
Zmiter <ozmiter@gmail.com>
2021-12-03 02:03:25 +0300

    feat: add user actions

--- src/resources/users/user.memory.repository.js
+++ src/resources/users/user.memory.repository.js
@@ -1,6 +1,44 @@
-const getAll = async () => {
-  // TODO: mock implementation. should be replaced during task development
-  return [];
+const inMemoryDB = require('../../inMemoryDB');
+const User = require('./user.model');
+
+const getAll = async () => inMemoryDB.users;
+
+const getUser = async (id) => {
+  const user = inMemoryDB.users.filter((item) => item.id === id)[0];
+  if (!user) {
+    throw new Error(`User with id ${id} not found`);
+  }
+  return User.toResponse(user);
 };
 
-module.exports = { getAll };
+const createUser = async (user) => {
+  const newUser = new User(user);
+  inMemoryDB.users.push(newUser);
+  return User.toResponse(newUser);
+};
+
+const updateUser = async (id, updateData) => {
+  if (inMemoryDB.users.findIndex((item) => item.id === id) === -1) {
+    throw new Error(`User with id ${id} not found`);
+  }
+  const userIndex = inMemoryDB.users.findIndex((item) => item.id === id);
+  let updatedUser = inMemoryDB.users[userIndex];
+  updatedUser = { ...updatedUser, ...updateData };
+  inMemoryDB.users[userIndex] = updatedUser;
+  return User.toResponse(updatedUser);
+};
+
+const deleteUser = (id) => {
+  if (inMemoryDB.users.findIndex((item) => item.id === id) === -1) {
+    throw new Error(`User with id ${id} not found`);
+  }
+  const filteredUsers = inMemoryDB.users.filter((item) => item.id !== id);
+  inMemoryDB.users = filteredUsers;
+
+// TODO delete user from tasks
+
+  return id;
+};
+
+
+module.exports = { getAll, getUser, createUser, deleteUser, updateUser };
--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -2,4 +2,12 @@ const usersRepo = require('./user.memory.repository');
 
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+const getUserByID = id => usersRepo.getUser(id);
+
+const createUser = user => usersRepo.createUser(user);
+
+const deleteUserByID = id => usersRepo.deleteUser(id);
+
+const updateUserByID = (id, updateData) => usersRepo.updateUser(id, updateData);
+
+module.exports = { getAll, getUserByID, createUser, deleteUserByID, updateUserByID  };
