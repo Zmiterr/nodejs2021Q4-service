a6c387fb4b2d3e880162a19b8dcd406581a8b273
Zmiter <ozmiter@gmail.com>
2021-12-10 17:21:16 +0300

    feat: add swagger

--- src/app.ts
+++ src/app.ts
@@ -7,27 +7,17 @@ const app = fastify({
   logger: true,
 });
 
-// server.register(require('fastify-swagger'), {
-//   exposeRoute: true,
-//   routePrefix: '/doc',
-//   mode: 'static',
-//   specification: {
-//     path: './doc/api.yaml',
-//   },
-//   swagger: {
-//     info: { title: 'fastify-api' },
-//   },
-// });
-
-// app.use('/doc', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
-
-// app.use('/', (req, res, next) => {
-//   if (req.originalUrl === '/') {
-//     res.send('Service is running!');
-//     return;
-//   }
-//   next();
-// });
+app.register(require('fastify-swagger'), {
+  exposeRoute: true,
+  routePrefix: '/doc',
+  mode: 'static',
+  specification: {
+    path: './doc/api.yaml',
+  },
+  swagger: {
+    info: { title: 'fastify-api' },
+  },
+});
 
 const routes = [...userRouter, ...boardRouter, ...taskRouter];
 routes.forEach((route) => {
@@ -35,14 +25,6 @@ routes.forEach((route) => {
   app.route(route);
 });
 
-// boardRouter.forEach((route) => {
-//   app.route(route);
-// });
-// taskRouter.forEach((route) => {
-//   app.route(route);
-// });
-
 app.get('/', async () => 'Service is running!');
 
-// module.exports = server;
 export default app;
