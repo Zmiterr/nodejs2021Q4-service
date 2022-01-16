e707516b32721e73a0a50cfbd9b35f58463d682e
Zmiter <ozmiter@gmail.com>
2022-01-16 20:14:16 +0300

    feat: add server connection

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,40 @@
-// const { PORT } = require('./common/config');
-// const app = require('./app');
+import { createConnection } from 'typeorm';
 import app, { logger } from './app';
+import 'reflect-metadata';
+import env from './common/config';
 
-import evn from './common/config';
-
+// const ormConfig = {
+//   type: 'postgres',
+//   host: 'postgres',
+//   port: env.POSTGRES_PORT,
+//   username: env.POSTGRES_USER,
+//   password: env.POSTGRES_PASSWORD,
+//   database: env.POSTGRES_DB,
+//   synchronize: true,
+//   entities: [
+//     'src/resources/users/user.model.ts',
+//     'src/resources/boards/board.model.ts',
+//     'src/resources/tasks/task.model.ts',
+//     'src/resources/columns/column.model.ts',
+//   ],
+// };
 (async () => {
   try {
-    await app.listen(evn.PORT, '0.0.0.0');
+    await createConnection({
+      type: 'postgres',
+      host: 'postgres',
+      port: Number(env.POSTGRES_PORT),
+      username: env.POSTGRES_USER,
+      password: env.POSTGRES_PASSWORD,
+      database: env.POSTGRES_DB,
+      synchronize: true,
+      entities: [
+        'src/resources/users/user.model.ts',
+        'src/resources/boards/board.model.ts',
+        'src/resources/tasks/task.model.ts',
+        'src/resources/columns/column.model.ts',
+      ],
+    }).then(() => app.listen(env.PORT, '0.0.0.0'));
   } catch (err) {
     app.log.error(err);
     logger.error(String(err));
