1b41c52bc93d5449e89cf5a75c4e80d2a2a1e31e
Zmiter <ozmiter@gmail.com>
2022-01-23 17:55:30 +0300

    feat: create user

--- src/migrations/1642625439429-name.ts
+++ src/migrations/1642625439429-name.ts
@@ -1,10 +1,10 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class name1642625439429 implements MigrationInterface {
-    name = 'name1642625439429'
+  name = 'name1642625439429';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             CREATE TABLE "user" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "name" character varying NOT NULL,
@@ -13,7 +13,7 @@ export class name1642625439429 implements MigrationInterface {
                 CONSTRAINT "PK_cace4a159ff9f2512dd42373760" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "board_column" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "title" character varying NOT NULL,
@@ -23,7 +23,7 @@ export class name1642625439429 implements MigrationInterface {
                 CONSTRAINT "PK_0273ece23af9b3e55ad6af2fdaa" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "task" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "title" character varying NOT NULL,
@@ -35,64 +35,68 @@ export class name1642625439429 implements MigrationInterface {
                 CONSTRAINT "PK_fb213f79ee45060ba925ecd576e" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             CREATE TABLE "board" (
                 "id" uuid NOT NULL DEFAULT uuid_generate_v4(),
                 "title" character varying NOT NULL,
                 CONSTRAINT "PK_865a0f2e22c140d261b1df80eb1" PRIMARY KEY ("id")
             )
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "board_column"
             ADD CONSTRAINT "FK_7d6b58efcc37a760ffd108eec72" FOREIGN KEY ("boardId") REFERENCES "board"("id") ON DELETE CASCADE ON UPDATE NO ACTION
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "board_column"
             ADD CONSTRAINT "FK_ee74fbd97194a322e7419214ea5" FOREIGN KEY ("tasksId") REFERENCES "task"("id") ON DELETE CASCADE ON UPDATE NO ACTION
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "task"
             ADD CONSTRAINT "FK_f316d3fe53497d4d8a2957db8b9" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE
             SET NULL ON UPDATE NO ACTION DEFERRABLE INITIALLY DEFERRED
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "task"
             ADD CONSTRAINT "FK_d88edac9d7990145ff6831a7bb3" FOREIGN KEY ("boardId") REFERENCES "board"("id") ON DELETE CASCADE ON UPDATE NO ACTION DEFERRABLE INITIALLY DEFERRED
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "task"
             ADD CONSTRAINT "FK_f56fe6f2d8ab0b970f764bd601b" FOREIGN KEY ("columnId") REFERENCES "board_column"("id") ON DELETE CASCADE ON UPDATE NO ACTION DEFERRABLE INITIALLY DEFERRED
         `);
-    }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`
+    await queryRunner.query(`
+            INSERT INTO "user" (name, login, password)
+                VALUES ('admin', 'admin', '$2a$07$I6lovwFvNSmSegctHRN86eMUrJDPlIl/nwRXjApL500sqNYRuOvd2')
+        `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
             ALTER TABLE "task" DROP CONSTRAINT "FK_f56fe6f2d8ab0b970f764bd601b"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "task" DROP CONSTRAINT "FK_d88edac9d7990145ff6831a7bb3"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "task" DROP CONSTRAINT "FK_f316d3fe53497d4d8a2957db8b9"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "board_column" DROP CONSTRAINT "FK_ee74fbd97194a322e7419214ea5"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             ALTER TABLE "board_column" DROP CONSTRAINT "FK_7d6b58efcc37a760ffd108eec72"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "board"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "task"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "board_column"
         `);
-        await queryRunner.query(`
+    await queryRunner.query(`
             DROP TABLE "user"
         `);
-    }
-
+  }
 }
