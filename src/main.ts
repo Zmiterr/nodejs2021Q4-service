ec4b08254ef50418e64d171ef1ca67faba0a824d
Zmiter <ozmiter@gmail.com>
2022-02-06 21:36:33 +0300

    feat: add fastify app support

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,33 @@
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AllExceptionsFilter } from './all-exception.filter';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const isUseFastify = process.env.USE_FASTIFY === 'true';
+  async function chooseFramework() {
+    if (isUseFastify) {
+      const fastifyAdapter = new FastifyAdapter();
 
-  const { httpAdapter } = app.get(HttpAdapterHost);
+      const app = await NestFactory.create<NestFastifyApplication>(
+        AppModule,
+        fastifyAdapter,
+      );
+      app.enableCors();
+      console.log('You has been started fastify app');
+      return app;
+    } else {
+      console.log('You has been started express app');
+      return await NestFactory.create(AppModule);
+    }
+  }
+  const app = await chooseFramework();
+  const { httpAdapter } = await app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
 
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
